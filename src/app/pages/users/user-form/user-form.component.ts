f4f8a7b52a08fb9931bfa2517da91a75fd5a18a6
ailson <ailsonlcosta@gmail.com>
2019-10-23 15:57:34 -0300

    ajustes no user

--- src/app/pages/users/user-form/user-form.component.ts
+++ src/app/pages/users/user-form/user-form.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { BaseResourceFormComponent } from '../../../shared/components/base-resource-form/base-resource-form.component';
+import { User } from '../models/user.model';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
-export class UserFormComponent implements OnInit {
+export class UserFormComponent extends BaseResourceFormComponent<User> {
 
-  constructor() { }
+  constructor(protected userService: UserService, protected injector: Injector) {
+    super(new User, injector, userService, User.fromJson);
+  }
 
-  ngOnInit() {
+  protected buildResourceForm() {
+    this.resourceForm = this.formBuilder.group({
+      id: [null],
+      username: [null, [Validators.required, Validators.minLength(2)]],
+      password: [null, [Validators.required, Validators.minLength(6)]],
+      status: [1, [Validators.required]]
+    });
+  }
+
+  protected editionPageTitle(): String {
+    const resourceUserName = this.resource.username || '';
+    return 'Editar: ' + resourceUserName;
   }
 
 }
